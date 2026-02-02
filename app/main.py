eeffd8043704fa9f36bba1efe4b643495dbd98fe
K N <niedbalaproject@gmail.com>
2026-02-02 13:56:39 +0100

    Add Car class

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 class Car:
     # write your code here
-    pass
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
+        comfort_class.self = comfort_class
+        clean_mark.self = clean_mark
+        brand.self = brand
 
 
 class CarWashStation:
