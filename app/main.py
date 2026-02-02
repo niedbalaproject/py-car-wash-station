4bfd251b5728e183a84205c8bd0458dd932e2548
K N <niedbalaproject@gmail.com>
2026-02-02 14:40:55 +0100

    Fix Car constructor attribute initialization

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 class Car:
     # write your code here
     def __init__(self, comfort_class: int, clean_mark: int, brand: str):
-        comfort_class.self = comfort_class
-        clean_mark.self = clean_mark
-        brand.self = brand
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
